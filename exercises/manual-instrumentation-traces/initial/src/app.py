59f02e1496c3647e8ac9b471c792ca0c39e255d9
Deven <75524130+Deven-14@users.noreply.github.com>
2025-11-08 06:14:47 +0000

    exercise: completed 07. Hands-On Lab: Manual Instrumentation - Traces

--- exercises/manual-instrumentation-traces/initial/src/app.py
+++ exercises/manual-instrumentation-traces/initial/src/app.py
@@ -4,12 +4,41 @@ import time
 
 import requests
 from client import ChaosClient, FakerClient
-from flask import Flask, make_response
+from flask import Flask, make_response, request
+
+from trace_utils import create_tracer
+
+from opentelemetry import trace as trace_api
+from opentelemetry.semconv.trace import SpanAttributes
+
+import json # <-- to pretty print response
+
+
+from opentelemetry import context
+from opentelemetry.propagate import inject, extract
+
+
 
 # global variables
 app = Flask(__name__)
+tracer = create_tracer("app.py", "0.1")
+
+
+@app.teardown_request
+def teardown_request_func(err):
+    previous_ctx = request.environ.get("previous_ctx_token", None)
+    if previous_ctx:
+        context.detach(previous_ctx)
+
+@app.before_request
+def before_request_func():
+    ctx = extract(request.headers)
+    previous_ctx = context.attach(ctx)
+    request.environ["previous_ctx_token"] = previous_ctx
+
 
 @app.route("/users", methods=["GET"])
+@tracer.start_as_current_span("users")
 def get_user():
     user, status = db.get_user(123)
     data = {}
@@ -19,15 +48,32 @@ def get_user():
     return response
 
 
+@tracer.start_as_current_span("do_stuff")
 def do_stuff():
     time.sleep(0.1)
+    headers = {}
+    inject(headers)
     url = "http://localhost:6000/"
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
+    
+    # debug
+    print("Headers included in outbound request:")
+    print(json.dumps(response.json()["request"]["headers"], indent=2))
+    
     return response
 
 
 @app.route("/")
+@tracer.start_as_current_span("index")
 def index():
+    span = trace_api.get_current_span()
+    span.set_attributes(
+        {
+            SpanAttributes.HTTP_REQUEST_METHOD: request.method,
+            SpanAttributes.URL_PATH: request.path,
+            SpanAttributes.HTTP_RESPONSE_STATUS_CODE: 200,
+        }
+    )
     do_stuff()
     current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
     return f"Hello, World! It's currently {current_time}"
